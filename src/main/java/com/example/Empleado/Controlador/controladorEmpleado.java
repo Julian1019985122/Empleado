ba9fabc14d0abf813bc8a28783cd956a9db9a3c7
Alejft18 <joseon85@gmail.com>
2023-03-30 08:09:41 -0500

    listar

--- src/main/java/com/example/Empleado/Controlador/controladorEmpleado.java
+++ src/main/java/com/example/Empleado/Controlador/controladorEmpleado.java
@@ -1,4 +1,19 @@
 package com.example.Empleado.Controlador;
+import com.example.Empleado.Empleado.Empleado;
+import org.springframework.web.bind.annotation.*;
+import com.example.Empleado.Controlador.controladorEmpleado;
+import com.example.Empleado.Servicios.servicioEmpleado;
 
+import java.util.ArrayList;
+
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
 public class controladorEmpleado {
+
+    servicioEmpleado servicio= new servicioEmpleado();
+
+    @GetMapping("/listarEmpleados")
+    public ArrayList<Empleado> listar(){return servicio.listar();}
+
 }
--- src/main/java/com/example/Empleado/Servicios/servicioEmpleado.java
+++ src/main/java/com/example/Empleado/Servicios/servicioEmpleado.java
@@ -1,4 +1,26 @@
 package com.example.Empleado.Servicios;
 
+import com.example.Empleado.Empleado.Empleado;
+
+import java.util.ArrayList;
+
 public class servicioEmpleado {
+
+
+ArrayList<Empleado> lista=new ArrayList<>();
+
+
+    public servicioEmpleado(){
+        lista.add(new Empleado(1011153,"julian","ospina","jefe",100000.0f,new int[]{1, 3, 1},new int[]{1, 3, 1},new int[]{1, 3, 1}));
+        lista.add(new Empleado(1564654,"julia","uribe","contratista",100000.0f,new int[]{1, 3, 1},new int[]{1, 3, 1},new int[]{1, 3, 1}));
+        lista.add(new Empleado(1556434,"juan","unibio","ayudante",100000.0f,new int[]{1, 3, 1},new int[]{1, 3, 1},new int[]{1, 3, 1}));
+        lista.add(new Empleado(1477684,"juana","alimaña","jefe de maquinaria",100000.0f,new int[]{1, 3, 1},new int[]{1, 3, 1},new int[]{1, 3, 1}));
+        lista.add(new Empleado(2651611,"julio","tocasucha","secretario",100000.0f,new int[]{1, 3, 1},new int[]{1, 3, 1},new int[]{1, 3, 1}));
+
+    }
+
+    public ArrayList<Empleado> listar(){
+        return lista;
+    }
+
 }
